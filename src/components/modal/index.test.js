31e7c2f8f8e0d9af51b8f20d5b776d092a024b9a
Vaishnavi Pawar <visha00116633@gmail.com>
2024-03-22 07:59:25 +0000

    modal - snapshot

---
+++ src/components/modal/index.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { describe, test, expect } from "@jest/globals";
+import Modal from "./index";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "../../store";
+
+
+function renderWithRedux(ui, store1 = store) {
+  return render(
+    <BrowserRouter>
+      <Provider store={store1}>{ui}</Provider>
+    </BrowserRouter>,
+  );
+
+}
+
+describe("renders modal correctly", () => {
+  it("should match the snapshot", () => {
+    const { asFragment } = renderWithRedux(
+      <Modal
+        body={"Test"}
+        onClose={()=>{}}
+      />,
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+}
+);
